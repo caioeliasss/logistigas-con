a800e70128b3b54396628e4ab1c5abf8f345a4e0
Caio Elias <caiocesar.elias01@gmail.com>
2026-03-26 14:21:18 -0300

    feat: add bicos data module and refactor scan.js to use it

--- scan.js
+++ scan.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const dotenv = require("dotenv")
+const bicos = require("./bicos")
+
 dotenv.config({ path: path.join(__dirname, ".env") })
 
 console.log("Script iniciado...")
@@ -17,24 +19,7 @@ if (!ip || !POSTO) {
   process.exit(1)
 }
 
-const BICOS = {
-  "84": { concentrador: "84", tanque: "001", bomba: "BOMBA 01", produto: "GASOLINA C COMUM" },
-  "44": { concentrador: "44", tanque: "002", bomba: "BOMBA 01", produto: "GASOLINA C ADITIVADA" },
-  "04": { concentrador: "04", tanque: "003", bomba: "BOMBA 01", produto: "ETANOL HIDRATADO COMUM" },
-  "85": { concentrador: "85", tanque: "001", bomba: "BOMBA 01", produto: "GASOLINA C COMUM" },
-  "45": { concentrador: "45", tanque: "002", bomba: "BOMBA 01", produto: "GASOLINA C ADITIVADA" },
-  "05": { concentrador: "05", tanque: "003", bomba: "BOMBA 01", produto: "ETANOL HIDRATADO COMUM" },
-  "88": { concentrador: "88", tanque: "001", bomba: "BOMBA 02", produto: "GASOLINA C COMUM" },
-  "48": { concentrador: "48", tanque: "002", bomba: "BOMBA 02", produto: "GASOLINA C ADITIVADA" },
-  "08": { concentrador: "08", tanque: "003", bomba: "BOMBA 02", produto: "ETANOL HIDRATADO COMUM" },
-  "89": { concentrador: "89", tanque: "001", bomba: "BOMBA 02", produto: "GASOLINA C COMUM" },
-  "49": { concentrador: "49", tanque: "002", bomba: "BOMBA 02", produto: "GASOLINA C ADITIVADA" },
-  "09": { concentrador: "09", tanque: "003", bomba: "BOMBA 02", produto: "ETANOL HIDRATADO COMUM" },
-  "0D": { concentrador: "0D", tanque: "005", bomba: "BOMBA 03", produto: "OLEO DIESEL B S500 - COMUM" },
-  "0C": { concentrador: "0C", tanque: "004", bomba: "BOMBA 03", produto: "OLEO DIESEL B S10 - COMUM" },
-  "10": { concentrador: "10", tanque: "005", bomba: "BOMBA 04", produto: "OLEO DIESEL B S500 - COMUM" },
-  "11": { concentrador: "11", tanque: "004", bomba: "BOMBA 04", produto: "OLEO DIESEL B S10 - COMUM" },
-}
+const BICOS = bicos
 
 async function main() {
   let koffi

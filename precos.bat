da01e7d3b9f0b0b7faee06b1ac756cef74a7ab80
Caio <caiocesar.elias01@gmail.com>
2026-03-31 13:18:12 -0300

    feat: simplify precos.bat by removing unused variables and improving API wait logic

--- precos.bat
+++ precos.bat
@@ -13,8 +13,6 @@ set "NODE_OUT=%ROOT%saida.log"
 set "NODE_ERR=%ROOT%erro.log"
 set "API_OUT=%ROOT%pumpapi.log"
 set "API_ERR=%ROOT%pumpapi-erro.log"
-set "API_STARTED_BY_SCRIPT="
-set "API_PID="
 
 if not exist "%NODE_EXE%" (
 	echo Node nao encontrado em "%NODE_EXE%".
@@ -44,8 +42,7 @@ if errorlevel 1 (
 
 	call :wait_for_api
 	if errorlevel 1 (
-		echo PumpApi nao respondeu em ate 30 segundos.
-		call :stop_api >nul 2>&1
+		echo PumpApi nao respondeu em ate 60 segundos.
 		exit /b 1
 	)
 ) else (
@@ -56,10 +53,6 @@ set "PUMP_API_URL=%API_URL%"
 "%NODE_EXE%" "%SCRIPT%" 1>"%NODE_OUT%" 2>"%NODE_ERR%"
 set "NODE_EXIT=%ERRORLEVEL%"
 
-if defined API_STARTED_BY_SCRIPT (
-	call :stop_api
-)
-
 exit /b %NODE_EXIT%
 
 :check_api
@@ -67,14 +60,7 @@ powershell -NoProfile -ExecutionPolicy Bypass -Command "try { $response = Invoke
 exit /b %ERRORLEVEL%
 
 :start_api
-for /f %%I in ('powershell -NoProfile -ExecutionPolicy Bypass -Command "$env:ASPNETCORE_URLS='%API_URL%'; $env:Companytec__DllPath='%DLL_PATH%'; $process = Start-Process -FilePath '%API_EXE%' -WorkingDirectory '%API_DIR%' -RedirectStandardOutput '%API_OUT%' -RedirectStandardError '%API_ERR%' -PassThru; $process.Id"') do set "API_PID=%%I"
-
-if not defined API_PID (
-	echo Falha ao iniciar PumpApi.
-	exit /b 1
-)
-
-set "API_STARTED_BY_SCRIPT=1"
+start "PumpApi" /b cmd /c "set ASPNETCORE_URLS=%API_URL%&& set Companytec__DllPath=%DLL_PATH%&& cd /d \"%API_DIR%\" && \"%API_EXE%\" 1>>\"%API_OUT%\" 2>>\"%API_ERR%\""
 exit /b 0
 
 :wait_for_api
@@ -88,9 +74,3 @@ if %API_WAIT_COUNT% GEQ 60 exit /b 1
 
 timeout /t 1 /nobreak >nul
 goto :wait_loop
-
-:stop_api
-if defined API_PID (
-	taskkill /PID %API_PID% /T /F >nul 2>&1
-)
-exit /b 0
